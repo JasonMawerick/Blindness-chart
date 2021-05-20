6ab4d3262835950f6c486d9abf0f05c14f5c4e49
Romain Poissonnier <r.poissonnier@codeur.online>
2021-05-20 13:54:08 +0200

    bliness

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <div id="charts">
+        <canvas id="browser" width="400" height="400"></canvas>
+        <canvas id="screen-reader" width="400" height="400"></canvas>
+        <canvas id="switch-tools" width="400" height="400"></canvas>
+        <canvas id="type-screen" width="400" height="400"></canvas>
+        <canvas id="phone-tools" width="400" height="400"></canvas>
+    </div>
+    
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.2.1/dist/chart.min.js"></script>
+    <script src="main.js"></script>
+</body>
+</html>
